5b6207739a4572ae3eba0f9fb165e48f12548ad1
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-25 00:28:34 +0100

    Create CRUD service functions for Part model

--- app/serializers/parts.py
+++ app/serializers/parts.py
@@ -2,7 +2,7 @@ from models.parts import Part
 from typing import Dict, Any
 
 
-def part_serializer(part: Part) -> Dict[str, Any]:
+def get_part_serializer(part: Part) -> Dict[str, Any]:
     return {
         "id": str(part["_id"]),
         "serial_number": part["serial_number"],
--- app/services/parts.py
+++ app/services/parts.py
@@ -1,13 +1,36 @@
 from typing import Dict, Any
 
+from fastapi import HTTPException
+
 from config.database import collection_parts
 from models.parts import Part
-from serializers.parts import part_serializer
+from serializers.parts import get_part_serializer
 
 
 def create_part_object(part: Part) -> Dict[str, Any]:
-    result = collection_parts.insert_one(dict(part))
-    inserted_id = result.inserted_id
-    inserted_part = collection_parts.find_one({"_id": inserted_id})
-    serialized_part = part_serializer(inserted_part)
-    return {"part": serialized_part}
+    _id = collection_parts.insert_one(part.dict()).inserted_id
+    inserted_part = collection_parts.find_one({"_id": _id})
+    return get_part_serializer(inserted_part)
+
+
+def get_part_object(serial_number: str) -> Dict[str, Any]:
+    inserted_part = collection_parts.find_one({"serial_number": serial_number})
+    if not inserted_part:
+        raise HTTPException(status_code=404, detail="Part not found")
+
+    return get_part_serializer(inserted_part)
+
+
+def update_part_object(serial_number: str, part: Part) -> Dict[str, Any]:
+    collection_parts.update_one({"serial_number": serial_number}, {"$set": part.dict()})
+    inserted_part = collection_parts.find_one({"serial_number": serial_number})
+    return get_part_serializer(inserted_part)
+
+
+def delete_part_object(serial_number: str) -> Dict[str, Any]:
+    part = collection_parts.find_one({"serial_number": serial_number})
+    if not part:
+        raise HTTPException(status_code=404, detail="Part not found")
+
+    collection_parts.delete_one({"serial_number": serial_number})
+    return get_part_serializer(part)
