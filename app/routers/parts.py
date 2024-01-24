ea9bc1f33c5378797a151bbc7c1556d8b418a7a1
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-25 00:45:06 +0100

    Parts move dependencies to routers

--- app/routers/parts.py
+++ app/routers/parts.py
@@ -1,7 +1,7 @@
 from typing import Dict, Any
 
-from fastapi import APIRouter
-
+from fastapi import APIRouter, Depends
+from config.database import get_parts_collection
 from models.parts import Part
 from services.parts import (
     create_part_object,
@@ -17,20 +17,20 @@ router = APIRouter(
 
 
 @router.post("/")
-def create_part(part: Part) -> Dict[str, Any]:
-    return create_part_object(part)
+def create_part(part: Part, collection: Any = Depends(get_parts_collection)) -> Dict[str, Any]:
+    return create_part_object(part, collection)
 
 
 @router.get("/{serial_number}")
-def get_part(serial_number: str) -> Dict[str, Any]:
-    return get_part_object(serial_number)
+def get_part(serial_number: str, collection: Any = Depends(get_parts_collection)) -> Dict[str, Any]:
+    return get_part_object(serial_number, collection)
 
 
 @router.post("/{serial_number}")
-def update_part(serial_number: str, part: Part) -> Dict[str, Any]:
-    return update_part_object(serial_number, part)
+def update_part(serial_number: str, part: Part, collection: Any = Depends(get_parts_collection)) -> Dict[str, Any]:
+    return update_part_object(serial_number, part, collection)
 
 
 @router.delete("/{serial_number}")
-def delete_part(serial_number: str) -> Dict[str, Any]:
-    return delete_part_object(serial_number)
+def delete_part(serial_number: str, collection: Any = Depends(get_parts_collection)) -> Dict[str, Any]:
+    return delete_part_object(serial_number, collection)
--- app/services/parts.py
+++ app/services/parts.py
@@ -2,37 +2,37 @@ from typing import Dict, Any
 
 from fastapi import HTTPException
 
-from config.database import get_parts_collection
 from models.parts import Part
 from serializers.parts import get_part_serializer
 
 
-def create_part_object(part: Part) -> Dict[str, Any]:
+def create_part_object(part: Part, collection: Any) -> Dict[str, Any]:
     # Todo ensure that part is not assigned to 'base' category
     # Todo part should be unique based on serial_number
-    _id = get_parts_collection().insert_one(part.dict()).inserted_id
-    inserted_part = get_parts_collection().find_one({"_id": _id})
+    _id = collection.insert_one(part.dict()).inserted_id
+    inserted_part = collection.find_one({"_id": _id})
     return get_part_serializer(inserted_part)
 
 
-def get_part_object(serial_number: str) -> Dict[str, Any]:
-    inserted_part = get_parts_collection().find_one({"serial_number": serial_number})
+def get_part_object(serial_number: str, collection: Any) -> Dict[str, Any]:
+    inserted_part = collection.find_one({"serial_number": serial_number})
     if not inserted_part:
         raise HTTPException(status_code=404, detail="Part not found")
 
     return get_part_serializer(inserted_part)
 
 
-def update_part_object(serial_number: str, part: Part) -> Dict[str, Any]:
-    get_parts_collection().update_one({"serial_number": serial_number}, {"$set": part.dict()})
-    inserted_part = get_parts_collection().find_one({"serial_number": serial_number})
+def update_part_object(serial_number: str, part: Part, collection: Any) -> Dict[str, Any]:
+    # Todo create model for update method
+    collection.update_one({"serial_number": serial_number}, {"$set": part.dict()})
+    inserted_part = collection.find_one({"serial_number": serial_number})
     return get_part_serializer(inserted_part)
 
 
-def delete_part_object(serial_number: str) -> Dict[str, Any]:
-    part = get_parts_collection().find_one({"serial_number": serial_number})
+def delete_part_object(serial_number: str , collection: Any) -> Dict[str, Any]:
+    part = collection.find_one({"serial_number": serial_number})
     if not part:
         raise HTTPException(status_code=404, detail="Part not found")
 
-    get_parts_collection().delete_one({"serial_number": serial_number})
+    collection.delete_one({"serial_number": serial_number})
     return get_part_serializer(part)
