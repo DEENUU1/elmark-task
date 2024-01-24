f2036026c203d9b8f972d3daaafbcdadc768780b
DEENUU1 <kacperwlodarczyk@protonmail.com>
2024-01-25 00:18:35 +0100

    Create endpoints for Category collection

--- app/routers/categories.py
+++ app/routers/categories.py
@@ -1,8 +1,12 @@
-from services.categories import create_category_object
-from models.categories import Category
-from typing import Dict, Optional
 from fastapi import APIRouter
 
+from models.categories import Category
+from services.categories import (
+    create_category_object,
+    get_category_object,
+    update_category_object,
+    delete_category_object
+)
 
 router = APIRouter(
     prefix="/api/v1/category",
@@ -13,3 +17,18 @@ router = APIRouter(
 @router.post("/")
 def create_category(category: Category):
     return create_category_object(category)
+
+
+@router.get("/{name}")
+def get_category(name: str):
+    return get_category_object(name)
+
+
+@router.put("/{name}")
+def update_category(name: str, category: Category):
+    return update_category_object(name, category)
+
+
+@router.delete("/{name}")
+def delete_category(name: str):
+    return delete_category_object(name)
--- app/services/categories.py
+++ app/services/categories.py
@@ -13,14 +13,14 @@ def create_category_object(category: Category) -> Dict[str, Optional[str]]:
     return get_category_serializer(inserted_category)
 
 
-def get_category_object(category_id: str) -> Dict[str, Optional[str]]:
-    inserted_category = collection_categories.find_one({"_id": category_id})
+def get_category_object(category_name: str) -> Dict[str, Optional[str]]:
+    inserted_category = collection_categories.find_one({"name": category_name})
     return get_category_serializer(inserted_category)
 
 
-def update_category_object(category_id: str, category: Category) -> Dict[str, Optional[str]]:
-    collection_categories.update_one({"_id": category_id}, {"$set": category.dict()})
-    inserted_category = collection_categories.find_one({"_id": category_id})
+def update_category_object(category_name: str, category: Category) -> Dict[str, Optional[str]]:
+    collection_categories.update_one({"category_name": category_name}, {"$set": category.dict()})
+    inserted_category = collection_categories.find_one({"category_name": category_name})
     return get_category_serializer(inserted_category)
 
 
